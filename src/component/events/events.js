9e6f129a499bdb8a34f4bb8d9823a1ea4d4b088c
Christopher Adie <adiec7@gmail.com>
2023-03-26 22:02:49 +0100

    added cooperate event

--- src/component/events/events.js
+++ src/component/events/events.js
@@ -2,6 +2,7 @@ import NavBar from "../navbar/navbar";
 import './events.css';
 import Footer from "../footer/footer";
 import Startupimg from "../img/startup.jpg";
+import Cooperateimg from "../img/cooperate.png";
 import { Card,CardGroup, Row,Col} from "react-bootstrap";
 const EventsPage = () => {
   return (
@@ -10,31 +11,31 @@ const EventsPage = () => {
       <div id="events_image">
         <div className="events_text lead">
           <h1>
-            <span>Kurious K</span> Events
+            <span className="pageSpan">Kurious K</span> Events
           </h1>
           <p className="lead">
-            Kurious K Events specializes in organizing corporate events and
-            industry-specific gatherings, bringing together experts to discuss
-            the latest trends, challenges, and opportunities. Let us help you
-            drive innovation and unlock new opportunities through our
-            knowledge-driven events.
+            <span className="pageSpan">Kurious K</span> Events specializes in
+            organizing corporate events and industry-specific gatherings,
+            bringing together experts to discuss the latest trends, challenges,
+            and opportunities. Let us help you drive innovation and unlock new
+            opportunities through our knowledge-driven events.
           </p>
-          <a className="btn btn-primary btn-lg" href="#t" role="button">
-            Sponsor events
+          <a className="btn btn-primary btn-lg" href="#events" role="button">
+            Check Events
           </a>
         </div>
       </div>
-      <section className="container mt-5 lead">
+      <section className="container mt-5 lead" id="events">
         <h2>Upcoming Events</h2>
 
         <CardGroup>
-          <Row xs={1} md={12} className="g-4">
+          <Row xs={1} md={2} className="g-4">
             <Col>
               <Card className="mb-5 card_text">
                 <Card.Img variant="top " src={Startupimg} className="starimg" />
                 <Card.Body>
                   <Card.Title>
-                    <h2>
+                    <h2 className="pageSpan">
                       {" "}
                       Kurious K Startup Investor Connect Event
                     </h2>
@@ -56,22 +57,34 @@ const EventsPage = () => {
                 </Card.Body>
               </Card>
             </Col>
-            {/* <Col>
-              <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+            <Col>
+              <Card className="mb-5 card_text">
+                <Card.Img variant="top " src={Cooperateimg} className="starimg" />
                 <Card.Body>
-                  <Card.Title>Card title</Card.Title>
+                  <Card.Title>
+                    <h2 className="pageSpan">
+                      {" "}
+                      Kurious K Corporate Innovation Event
+                    </h2>
+                  </Card.Title>
                   <Card.Text>
-                    This card has supporting text below as a natural lead-in to
-                    additional content.{" "}
+                    Kurious K Corporate Innovation Event aims to explore how
+                    corporate can drive support for the startup ecosystem in
+                    Africa and discuss the future of innovation on the
+                    continent.
                   </Card.Text>
+                  <a
+                    className="btn card-btn btn-lg"
+                    href="#n"
+                    role="button"
+                    rel="noreferrer"
+                  >
+                    Coming Soon
+                  </a>
                 </Card.Body>
-                <Card.Footer>
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
               </Card>
             </Col>
-            <Col>
+            {/* <Col>
               <Card>
                 <Card.Img variant="top" src="holder.js/100px160" />
                 <Card.Body>
@@ -90,7 +103,7 @@ const EventsPage = () => {
           </Row>
         </CardGroup>
       </section>
-      <Footer/>
+      <Footer />
     </>
   );
 };
