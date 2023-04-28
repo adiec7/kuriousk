c5a3386073597a8c8a12eee7bf45418988c57917
Christopher Adie <adiec7@gmail.com>
2023-04-28 11:54:58 +0100

    added podcastr episodes img

--- src/component/podcast/Podcast.js
+++ src/component/podcast/Podcast.js
@@ -2,7 +2,7 @@ import { React } from "react";
 import NavBar from "../navbar/navbar";
 import Podcastimg from "../img/podcast.jpg";
 import './podcast.css'
-import { Dropdown } from "react-bootstrap";
+import { DropdownButton,Dropdown, Button} from "react-bootstrap";
 import Footer from "../footer/footer";
 import EpisodePodcast from "./episode";
 export const Podcast = () => {
@@ -16,32 +16,16 @@ export const Podcast = () => {
           <h1>
             Welcome to <span className="pageSpan">kurious k </span>podcast
           </h1>
-          <Dropdown>
-            <Dropdown.Toggle className="btn-lg" id="dropdown-basic">
-              Listen Here
-            </Dropdown.Toggle>
+          <Button variant="info" href="#episodes" >View Episodes</Button>
 
-            <Dropdown.Menu>
-              <Dropdown.Item
-                href="https://open.spotify.com/show/42YnMzOWb923EkxZ8kU0RV?go=1&sp_cid=dfd0bd0e-3ac2-4f64-9668-7e410c5364a7&utm_source=embed_player_p&utm_medium=desktop&nd=1"
-                target="_blank"
-              >
-                Spotify
-              </Dropdown.Item>
-              <Dropdown.Item
-                href="https://podcasts.apple.com/ng/podcast/kurious-k/id1573065694"
-                target="_blank"
-              >
-                Apple Podcast
-              </Dropdown.Item>
-              <Dropdown.Item
-                href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy82MDJiNzJlOC9wb2RjYXN0L3Jzcw=="
-                target="_blank"
-              >
-                Google Podcast
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <DropdownButton variant="info" size="md" id="dropdown-basic-button" title="Listen to Podcast">
+      <Dropdown.Item  href="https://open.spotify.com/show/42YnMzOWb923EkxZ8kU0RV?go=1&sp_cid=dfd0bd0e-3ac2-4f64-9668-7e410c5364a7&utm_source=embed_player_p&utm_medium=desktop&nd=1" target="_blank" rel="noreferrer">Spotify</Dropdown.Item>
+      <Dropdown.Item href="https://podcasts.apple.com/ng/podcast/kurious-k/id1573065694"
+                target="_blank" rel="noreferrer">Apple Podcast</Dropdown.Item>
+      <Dropdown.Item href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy82MDJiNzJlOC9wb2RjYXN0L3Jzcw=="
+                target="_blank" rel="noreferrer">Google Podcast</Dropdown.Item>
+    </DropdownButton>
+         
         </div>
       </div>
       <EpisodePodcast/>
--- src/component/podcast/episode.js
+++ src/component/podcast/episode.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Form } from 'react-bootstrap';
 
 function EpisodePodcast() {
   const [episodes, setEpisodes] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [episodesPerPage] = useState(6);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch('https://anchor.fm/s/602b72e8/podcast/rss')
@@ -12,39 +15,84 @@ function EpisodePodcast() {
         const xml = parser.parseFromString(data, 'application/xml');
         const items = xml.querySelectorAll('item');
         const episodeData = Array.from(items).map(item => {
+          const image = item.querySelector('itunes\\:image') ? item.querySelector('itunes\\:image').getAttribute('href') : xml.querySelector('channel image url').textContent;
           return {
             title: item.querySelector('title').textContent,
             description: item.querySelector('description').textContent,
             audio: item.querySelector('enclosure').getAttribute('url'),
-            spotify: item.querySelector('guid').textContent,
-            itunes: item.querySelector('link').textContent
+            spotify: item.querySelector('link').textContent,
+            itunes: item.querySelector('guid').textContent,
+            google: item.querySelector('google\\:play') ? item.querySelector('google\\:play').getAttribute('url') : null,
+            image: image
           }
         });
         setEpisodes(episodeData);
       });
   }, []);
 
+  const openLink = (link) => {
+    window.location.href = link;
+  }
+
+  const indexOfLastEpisode = currentPage * episodesPerPage;
+  const indexOfFirstEpisode = indexOfLastEpisode - episodesPerPage;
+  const currentEpisodes = episodes.filter(episode =>
+    episode.title.toLowerCase().includes(searchTerm.toLowerCase())
+  ).slice(indexOfFirstEpisode, indexOfLastEpisode);
+
+  const renderEpisodes = () => {
+    return currentEpisodes.map((episode, index) => (
+      <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index} >
+        <Card>
+          <Card.Img variant="top" src={episode.image} />
+          <Card.Body>
+            <Card.Title>{episode.title}</Card.Title>
+            <Card.Text className="text-truncate" style={{maxHeight: '3em', overflow: 'hidden'}}>{episode.description}</Card.Text>
+            <audio controls src={episode.audio} preload="none" />
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="info" onClick={() => openLink(episode.spotify)}>Listen on Spotify</Button>{' '}
+            <Button variant="secondary" onClick={() => openLink(episode.itunes)}>Listen on iTunes</Button>
+            {episode.google && <Button variant="success" onClick={() => openLink(episode.google)}>Listen on Google Podcasts</Button>}
+          </Card.Footer>
+        </Card>
+      </div>
+    ));
+  }
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    setCurrentPage(1);
+  }
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+  }
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  }
+
   return (
-    <div className="container mt-5">
+    <div className="container " id='episodes'>
+      <div className="row justify-content-center mt-4 mb-3">
+        <Form.Group controlId="searchEpisodes">
+          <Form.Control type="text" placeholder="Search Episodes" value={searchTerm} onChange={handleSearch} />
+        </Form.Group>
+      </div>
       <div className="row justify-content-center">
-        {episodes.map((episode, index) => (
-          <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={index}>
-            <Card>
-              <Card.Body>
-                <Card.Title>{episode.title}</Card.Title>
-                <Card.Text className="text-truncate" style={{maxHeight: '3em', overflow: 'hidden'}}>{episode.description}</Card.Text>
-                <audio controls src={episode.audio} preload="none" />
-              </Card.Body>
-              <Card.Footer>
-                <Button className='btn-lg' href={episode.itunes} target="_blank" rel="noopener noreferrer">Listen on Spotify</Button>
-                <Button variant="secondary" href={episode.spotify} target="_blank" rel="noopener noreferrer">Listen on iTunes</Button>
-              </Card.Footer>
-            </Card>
-          </div>
-        ))}
+        {renderEpisodes()}
+      </div>
+      <div className=" justify-content-center mb-5">
+        <Button className='btn-lg' onClick={handlePrevPage} disabled={currentPage === 1}>Previous</Button>{' '}
+        <Button className="btn-lg" onClick={handleNextPage} disabled={currentEpisodes.length < episodesPerPage}>Next</Button>
       </div>
     </div>
   );
 }
 
 export default EpisodePodcast;
+
+
+
+
